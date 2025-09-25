20306c6243224482a31895bc94dcdf04af0e203c
Lost004 <nazimri@code.edu.az>
2025-09-26 02:23:22 +0400

    ckeditor

--- src/i18n.js
+++ src/i18n.js
@@ -26,6 +26,26 @@ const messages = {
     privacy_policy: "Məxfilik siyasəti",
     customer_support: "Müştəri dəstəyi",
     terms_conditions: "Şərtlər və qaydalar",
+    comment: "Şərh",
+    comments: "Şərhlər",
+    startDiscussion: "Müzakirəyə başlayın...",
+    bold: "Qalın",
+    italic: "İtalik",
+    underline: "Altı xətli",
+    link: "Link",
+    gif: "GIF",
+    loginWith: "DAXİL OLUN",
+    name: "Ad",
+    email: "E-poçt",
+    password: "Şifrə",
+    disqusNote:
+      "Bu şərh platforması Disqus, Inc tərəfindən yerləşdirilir. Mən Disqus və tərəfdaşlarına icazə verirəm ki...",
+    acknowledge18: "18 yaşdan yuxarı olduğumu təsdiqləyirəm",
+    postAsGuest: "Qonaq kimi paylaşmaq istəyirəm",
+    share: "Paylaş",
+    best: "Ən yaxşı",
+    newest: "Ən yeni",
+    oldest: "Ən köhnə",
     footer_rights: "Bütün hüquqlar qorunur.",
     subscribe_now: "İndi abunə ol",
     authors: "Müəlliflər",
@@ -35,24 +55,29 @@ const messages = {
     see_more: "Daha çoxunu gör",
     by: "Tərəfindən",
     no_posts_for: "Bu kateqoriya üçün heç bir paylaşım yoxdur",
-  privacy_policy: "Məxfilik siyasəti",
+    privacy_policy: "Məxfilik siyasəti",
     who_we_are: "Biz kimik?",
     website_address: "Veb sayt ünvanımız: http://hashthemes.com",
     loading_sections: "Əlavə bölmələr yüklənir…",
-    dynamic_content_note: "Bu məzmun dinamikdir: <code>endpoint</code> prop-u vasitəsilə backend-dən JSON qaytarın.",
-    
+    dynamic_content_note:
+      "Bu məzmun dinamikdir: <code>endpoint</code> prop-u vasitəsilə backend-dən JSON qaytarın.",
+
     embedded_title: "Başqa vebsaytlardan daxil edilmiş məzmun",
-    embedded_body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Embedded content (məsələn videolar, xəritələr) başqa vebsaytlardan davranır ki, ziyarətçi həmin vebsayta daxil olubmuş kimi.",
-    
+    embedded_body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Embedded content (məsələn videolar, xəritələr) başqa vebsaytlardan davranır ki, ziyarətçi həmin vebsayta daxil olubmuş kimi.",
+
     retention_title: "Məlumatlarınızı nə qədər saxlayırıq",
-    retention_body: "Şəxsi məlumatları yalnız xidmət göstərmək, hüquqi öhdəlikləri yerinə yetirmək, mübahisələri həll etmək və razılaşmaları tətbiq etmək üçün lazım olduğu müddətcə saxlayırıq.",
-    
+    retention_body:
+      "Şəxsi məlumatları yalnız xidmət göstərmək, hüquqi öhdəlikləri yerinə yetirmək, mübahisələri həll etmək və razılaşmaları tətbiq etmək üçün lazım olduğu müddətcə saxlayırıq.",
+
     rights_title: "Məlumatlarınız üzərində hansı hüquqlarınız var",
-    rights_body: "Məlumatlarınıza daxil olmaq, düzəliş etmək, ixrac etmək və silmək hüququnuz var. Müəyyən hallarda emalın qarşısını almaq və ya məhdudlaşdırmaq hüququna sahibsiniz.",
-    
+    rights_body:
+      "Məlumatlarınıza daxil olmaq, düzəliş etmək, ixrac etmək və silmək hüququnuz var. Müəyyən hallarda emalın qarşısını almaq və ya məhdudlaşdırmaq hüququna sahibsiniz.",
+
     transfers_title: "Məlumatlarınızı haraya göndəririk",
-    transfers_body: "Məlumatlarınız ölkənizdən kənarda bir serverə göndərilə və saxlanıla bilər. Biz lazımi qoruyucu tədbirləri təmin edirik.",
-  
+    transfers_body:
+      "Məlumatlarınız ölkənizdən kənarda bir serverə göndərilə və saxlanıla bilər. Biz lazımi qoruyucu tədbirləri təmin edirik.",
+
     terms_conditions: "Şərtlər və qaydalar",
     terms_intro:
       "Bu səhifədə “siz” dedikdə, alıcı nəzərdə tutulur. “Biz” dedikdə isə Bunzo komandası nəzərdə tutulur.",
@@ -68,7 +93,20 @@ const messages = {
       "# Yalnız məhsul zədələnmişsə geri ödəniş ediləcək. Məhsulun zədələnib-zədələnmədiyini öyrənmək üçün bizimlə əlaqə saxlayın.",
       "# Saytın siyasəti, şərtləri, paketləri, qiymətləri, xüsusiyyətləri və digər məlumatları dəyişdirmək hüququmuz var.",
       "# Saytın istifadəçisi olaraq, şərtlər və qaydalarla razılaşmalısınız.",
-      "# Şərtlərdə hər hansı dəyişiklik etsək, burada yerləşdirəcəyik."
+      "# Şərtlərdə hər hansı dəyişiklik etsək, burada yerləşdirəcəyik.",
+    ],
+  
+      section1: `Lorem Ipsum mətinin bir çox variasiyaları mövcuddur, lakin əksəriyyəti dəyişdirilmişdir, gülməli əlavələr və ya təsadüfi sözlər əlavə edilmişdir ki, bu da inandırıcı görünmür. Əgər Lorem Ipsum istifadə edəcəksinizsə, mətnin ortasında xoşagəlməz heç nə olmadığından əmin olmalısınız. İnternetdəki bütün Lorem Ipsum generatorları lazım olduqda əvvəlcədən müəyyən edilmiş parçaları təkrar edir, bu da onu ilk həqiqi generator edir.`,
+
+    title: `Gözəl dizaynlar yaradın və daha çox müştəri qazanın`,
+
+    section2: `Lorem Ipsum mətnlərinin bir çoxu mövcuddur, lakin əksəriyyəti dəyişdirilmişdir, gülməli əlavələr və ya təsadüfi sözlər əlavə edilmişdir ki, bu da inandırıcı görünmür. Əgər Lorem Ipsum istifadə edəcəksinizsə, mətnin ortasında xoşagəlməz heç nə olmadığından əmin olun.`,
+
+    list: [
+      `mətnin ortasında xoşagəlməz heç nə olmadığından əmin olun. İnternetdəki bütün Lorem Ipsum generatorları əvvəlcədən müəyyən edilmiş parçaları təkrar edir`,
+      `mətnin ortasında xoşagəlməz heç nə olmadığından əmin olun. Bütün Lorem Ipsum`,
+      `mətnin ortasında xoşagəlməz heç nə. Bütün Lorem Ipsum generatorları əvvəlcədən müəyyən edilmiş parçaları təkrar edir`,
+      `mətnin ortasında xoşagəlməz heç nə olmadığından əmin olun. İnternetdəki bütün Lorem Ipsum generatorları əvvəlcədən müəyyən edilmiş parçaları təkrar edir`,
     ],
     // Contact.vue
     fullName: "Ad Soyad",
@@ -87,7 +125,6 @@ const messages = {
     and: "və",
     write: "yazmaq",
     with: "ilə",
-    
 
     // BlogCard tagları
     tags: {
@@ -96,7 +133,7 @@ const messages = {
       design: "Dizayn",
       joomla: "Joomla",
       magento: "Magento",
-      wordpress: "WordPress"
+      wordpress: "WordPress",
     },
 
     // FAQ
@@ -111,9 +148,6 @@ const messages = {
     a4: "Sed a turpis nec urna hendrerit malesuada. Pellentesque habitant morbi...",
     q5: "Rəqəmlər təcrübələr üçün uyğundur?",
     a5: "Curabitur at fringilla justo. Donec eu varius sem, nec sagittis risus...",
-
-
-    
 
     // Tarixlər
     dates: {
@@ -145,7 +179,7 @@ const messages = {
     pages: "Pages",
     allpost: "All Posts",
     login: "Login",
-     terms: "Terms",
+    terms: "Terms",
     register: "Register",
     "already-have-account": "Already have an account?",
     "dont-have-account": "Don't have an account?",
@@ -154,6 +188,26 @@ const messages = {
     faq: "FAQ",
     careers: "Careers",
     about_us: "About Us",
+    comment: "Comment",
+    comments: "Comments",
+    startDiscussion: "Start the discussion...",
+    bold: "Bold",
+    italic: "Italic",
+    underline: "Underline",
+    link: "Link",
+    gif: "GIF",
+    loginWith: "LOG IN WITH",
+    name: "Name",
+    email: "Email",
+    password: "Password",
+    disqusNote:
+      "This comment platform is hosted by Disqus, Inc. I authorize Disqus and its affiliates to...",
+    acknowledge18: "Acknowledge I am 18 or older",
+    postAsGuest: "I'd rather post as a guest",
+    share: "Share",
+    best: "Best",
+    newest: "Newest",
+    oldest: "Oldest",
     contact_us: "Contact Us",
     quick_links: "Quick Links",
     discussion: "Discussion",
@@ -161,7 +215,8 @@ const messages = {
     privacy_policy: "Privacy Policy",
     customer_support: "Customer Support",
     terms_conditions: "Terms & Conditions",
-    footer_rights: "All rights reserved © 2023 Bunzo Made with ❤ by Codecarnival",
+    footer_rights:
+      "All rights reserved © 2023 Bunzo Made with ❤ by Codecarnival",
     subscribe_now: "Subscribe Now",
     authors: "From Author's",
     time: "min read",
@@ -174,20 +229,25 @@ const messages = {
     who_we_are: "Who we are?",
     website_address: "Our website address is: http://hashthemes.com",
     loading_sections: "Loading additional sections…",
-    dynamic_content_note: "This content is dynamic: pass a backend endpoint via the <code>endpoint</code> prop that returns JSON.",
-    
+    dynamic_content_note:
+      "This content is dynamic: pass a backend endpoint via the <code>endpoint</code> prop that returns JSON.",
+
     embedded_title: "Embedded content from other websites",
-    embedded_body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Embedded content (e.g. videos, maps) from other websites behaves as if the visitor has visited the other website.",
-    
+    embedded_body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Embedded content (e.g. videos, maps) from other websites behaves as if the visitor has visited the other website.",
+
     retention_title: "How long we retain your data",
-    retention_body: "We retain personal data only for as long as necessary to provide services, comply with legal obligations, resolve disputes and enforce agreements.",
-    
+    retention_body:
+      "We retain personal data only for as long as necessary to provide services, comply with legal obligations, resolve disputes and enforce agreements.",
+
     rights_title: "What rights you have over your data",
-    rights_body: "You have the right to access, correct, export, and delete your personal data. You can object to or restrict processing in certain circumstances.",
-    
+    rights_body:
+      "You have the right to access, correct, export, and delete your personal data. You can object to or restrict processing in certain circumstances.",
+
     transfers_title: "Where we send your data",
-    transfers_body: "Your data may be transferred to and stored at a destination outside your country. We take appropriate safeguards.",
-      terms_intro:
+    transfers_body:
+      "Your data may be transferred to and stored at a destination outside your country. We take appropriate safeguards.",
+    terms_intro:
       "When we’re saying “you” in this page, we’re referring to a buyer. When we’re saying “we” in this page, we’re referring to the Bunzo authority.",
 
     terms_list: [
@@ -201,9 +261,20 @@ const messages = {
       "# We will refund only if the product is broken. To verify this, contact us.",
       "# We have the right to change policy, terms & conditions, packages, prices, features, or any other content on this website.",
       "# As a customer/user, you must agree with the terms & conditions.",
-      "# If we make any change in terms & conditions, we will post here."
+      "# If we make any change in terms & conditions, we will post here.",
     ],
+        section1: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn’t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.`,
 
+    title: `Create beautiful designs that will help convert more customers`,
+
+    section2: `passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn’t anything embarrassing hidden in the middle of text.`,
+
+    list: [
+      `you need to be sure there isn’t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary`,
+      `you need to be sure there isn’t anything embarrassing hidden in the middle of text. All the Lorem Ipsum`,
+      `embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined`,
+      `you need to be sure there isn’t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary`,
+    ],
     // Contact.vue
     fullName: "Full Name",
     email: "Email",
@@ -211,7 +282,6 @@ const messages = {
     message: "Message",
     writeMessage: "Write your message",
     sendMessage: "Send Message",
-    
 
     // About.vue
     openPlatform: "Open Platform",
@@ -230,7 +300,7 @@ const messages = {
       design: "Design",
       joomla: "Joomla",
       magento: "Magento",
-      wordpress: "WordPress"
+      wordpress: "WordPress",
     },
 
     // FAQ
--- src/views/BlogDetail.vue
+++ src/views/BlogDetail.vue
@@ -3,9 +3,11 @@
     <!-- Tabs / Breadcrumb -->
     <div class="tabs-container mb-4">
       <div class="tabs">
-        <router-link to="/" class="tab-link">{{ $t("home") }}</router-link>
-        <router-link to="/all-posts" class="tab-link">{{ $t("blog") }}</router-link>
-        <span class="tab-link active">{{ $t(post.title) }}</span>
+        <router-link to="/" class="tab-link">{{ t("home") }}</router-link>
+        <router-link to="/all-posts" class="tab-link">{{
+          t("blog")
+        }}</router-link>
+        <span class="tab-link active">{{ t(post.title) }}</span>
       </div>
     </div>
 
@@ -13,17 +15,17 @@
     <div
       class="post-meta d-flex justify-content-center gap-3 mb-2 flex-wrap text-center"
     >
-      <span>{{ $t("by") }} {{ $t(post.author) }}</span>
-      <span>📅 {{ $t(`dates.${post.date}`) }}</span>
-      <span>⏱ {{ post.readTime }} {{ $t("time") }}</span>
+      <span>{{ t("by") }} {{ t(post.author) }}</span>
+      <span>📅 {{ t(`dates.${post.date}`) }}</span>
+      <span>⏱ {{ post.readTime }} {{ t("time") }}</span>
     </div>
 
     <!-- Title -->
-    <h1 class="post-title mb-2 text-center">{{ $t(post.title) }}</h1>
+    <h1 class="post-title mb-2 text-center">{{ t(post.title) }}</h1>
 
     <!-- Tag -->
     <div class="tag-container mb-4">
-      <span class="post-tag">{{ $t(`tags.${post.tag.toLowerCase()}`) }}</span>
+      <span class="post-tag">{{ t(`tags.${post.tag.toLowerCase()}`) }}</span>
     </div>
 
     <!-- Image -->
@@ -33,14 +35,25 @@
       class="img-fluid mb-4 mx-auto d-block"
     />
 
-    <!-- Content -->
-    <div class="post-content">
-      <p v-html="post.content"></p>
-    </div>
+  <div class="text-section">
+        <p>{{ $t('section1') }}</p>
 
+    <h2 class="font-bold text-xl mt-4 mb-2">
+      {{ $t('title') }}
+    </h2>
+
+    <p>{{ $t('section2') }}</p>
+
+    <!-- Burada artıq massivlə işləyirik -->
+    <ul class="list-disc pl-6 mt-3 space-y-2">
+      <li v-for="(item, index) in $tm('list')" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
     <!-- Share Buttons -->
     <div class="share-container mt-4">
-      <span class="share-title">{{ $t("share") }}:</span>
+      <span class="share-title">{{ t("share") }}:</span>
       <div class="share-buttons">
         <a
           v-for="(item, index) in socials"
@@ -53,9 +66,14 @@
         </a>
       </div>
     </div>
-
     <!-- CKEditor -->
-    <CommentBox />
+    <div class="post-content mb-4">
+      <ckeditor
+        :editor="editor"
+        v-model="editorData"
+        :disabled="true"
+      ></ckeditor>
+    </div>
   </div>
 
   <div v-else class="container my-5 text-center">
@@ -64,68 +82,67 @@
 </template>
 
 <script>
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
+import { useI18n } from "vue-i18n";
 import img1 from "@/assets/img/1-2.jpg";
 import img2 from "@/assets/img/2-1.jpg";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-vue";
-import CommentBox from "@/components/CommentBox.vue";
 
 export default {
   name: "BlogDetail",
-  components: { CKEditor, CommentBox },
-  data() {
-    return {
-      post: null,
-      editor: ClassicEditor,
-      editorData: "",
+  components: { CKEditor},
+  setup() {
+    const route = useRoute();
+    const { t } = useI18n({ useScope: "global" });
 
-      posts: [
-        {
-          id: 1,
-          title: "title1", // i18n açarı
-          content: "<p>Full post content here...</p>",
-          tag: "javascript",
-          image: img1,
-          author: "authors", // i18n açarı
-          date: "apr12", // i18n dates içində açar
-          readTime: 3,
-        },
-        {
-          id: 2,
-          title: "title2",
-          content: "<p>Full post content here...</p>",
-          tag: "magento",
-          image: img2,
-          author: "authors",
-          date: "apr13",
-          readTime: 5,
-        },
-      ],
+    const posts = [
+      {
+        id: 1,
+        title: "title1",
+        tag: "javascript",
+        image: img1,
+        author: "authors",
+        date: "apr12",
+        readTime: 3,
+      },
+      {
+        id: 2,
+        title: "title2",
+        tag: "magento",
+        image: img2,
+        author: "authors",
+        date: "apr13",
+        readTime: 5,
+      },
+    ];
 
-      socials: [
-        {
-          name: "facebook",
-          icon: "fab fa-facebook-f",
-          link: "https://facebook.com",
-        },
-        {
-          name: "twitter",
-          icon: "fab fa-twitter",
-          link: "https://twitter.com",
-        },
-        {
-          name: "linkedin",
-          icon: "fab fa-linkedin-in",
-          link: "https://linkedin.com",
-        },
-        { name: "skype", icon: "fab fa-skype", link: "https://skype.com" },
-      ],
-    };
-  },
-  created() {
-    const postId = parseInt(this.$route.params.id);
-    this.post = this.posts.find((p) => p.id === postId);
-    if (this.post) this.editorData = this.post.content;
+    const post = ref(posts.find((p) => p.id === parseInt(route.params.id)));
+    const editor = ClassicEditor;
+
+    // CKEditor məzmunu i18n açarına uyğun
+    const editorData = computed({
+      get: () => (post.value ? t(`${post.value.title}_content`) : ""),
+      set: () => {},
+    });
+
+    const socials = [
+      {
+        name: "facebook",
+        icon: "fab fa-facebook-f",
+        link: "https://facebook.com",
+      },
+      { name: "twitter", icon: "fab fa-twitter", link: "https://twitter.com" },
+      {
+        name: "linkedin",
+        icon: "fab fa-linkedin-in",
+        link: "https://linkedin.com",
+      },
+      { name: "skype", icon: "fab fa-skype", link: "https://skype.com" },
+    ];
+
+    return { post, editor, editorData, socials, t };
   },
 };
 </script>
@@ -170,6 +187,7 @@ export default {
   font-size: 32px;
   color: #222;
 }
+
 .tag-container {
   display: flex;
   justify-content: center;
@@ -193,7 +211,7 @@ img {
 }
 
 /* Content */
-.post-content p {
+.post-content {
   font-size: 16px;
   line-height: 1.8;
   color: #444;
@@ -209,6 +227,7 @@ img {
   align-items: center;
   justify-content: space-between;
   font-family: Arial, sans-serif;
+  margin-bottom: 40px;
 }
 .share-title {
   font-weight: 600;
@@ -235,11 +254,65 @@ img {
   background: #e0e0e0;
 }
 
+
+/* CKEditor Post Content */
+.post-content .ck-editor__editable_inline {
+  min-height: 250px; /* daha rahat oxumaq üçün */
+  padding: 15px;
+  border-radius: 12px;
+  border: 1px solid #e0e0e0;
+  background: #fafafa;
+  font-size: 16px;
+  line-height: 1.8;
+  color: #333;
+}
+
+/* toolbar görünməsin (read-only üçün) */
+.post-content .ck-editor__top {
+  display: none;
+}
+
+/* scroll lazım olsa */
+.post-content .ck-editor__editable_inline {
+  overflow-y: auto;
+  max-height: 500px;
+}
+
+/* paragraph spacing */
+.post-content .ck-content p {
+  margin-bottom: 16px;
+}
+
+/* headings */
+.post-content .ck-content h1,
+.post-content .ck-content h2,
+.post-content .ck-content h3 {
+  margin-top: 20px;
+  margin-bottom: 12px;
+  font-weight: 700;
+  color: #222;
+}
+
+/* links */
+.post-content .ck-content a {
+  color: #1a73e8;
+  text-decoration: underline;
+}
+
+/* blockquote */
+.post-content .ck-content blockquote {
+  border-left: 3px solid #ccc;
+  padding-left: 12px;
+  color: #666;
+  font-style: italic;
+  margin: 12px 0;
+}
+
 @media (max-width: 768px) {
   .post-title {
     font-size: 26px;
   }
-  .post-content p {
+  .post-content {
     font-size: 15px;
   }
   .tabs-container {
